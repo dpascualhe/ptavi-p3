d57e7888fbbad600155bbc649ac18ecce2e83e0b
dpascualhe <d.pascualhe@gmail.com>
2014-10-12 19:53:04 +0200

    imprime ordenado

--- karaoke.py
+++ karaoke.py
@@ -3,4 +3,28 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import samllsmilhandler
+from smallsmilhandler import SmallSMILHandler 
+import sys
+
+
+try:
+    fichero = sys.argv[1]
+except IndexError:
+    print 'Usage: python karaoke.py file.smil'
+    raise SystemExit
+
+parser = make_parser()
+sHandler = SmallSMILHandler()
+parser.setContentHandler(sHandler)
+parser.parse(open(fichero))
+
+tags = sHandler.tags_list
+
+for tag in tags:
+    t_name = tag.keys()[0]
+    full_tag = t_name
+    for attr in tag[t_name]:
+        value = tag[t_name][attr] 
+        if value != "":
+            full_tag += '\t' + attr + '=' + value
+    print full_tag
