fe0bbed3e6e43ac0328bfaeb9b2e33cb43a89e12
david pascual hernandez <dpascual@iota42.aulas.gsyc.es>
2014-10-13 11:51:34 +0200

    definitivo

--- karaoke.py
+++ karaoke.py
@@ -3,48 +3,51 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-from smallsmilhandler import SmallSMILHandler 
+from smallsmilhandler import SmallSMILHandler
 import sys
 import os
 
+
 class KaraokeLocal():
-    
+
     def __init__(self, fich):
         parser = make_parser()
         sHandler = SmallSMILHandler()
         parser.setContentHandler(sHandler)
         parser.parse(open(fichero))
         self.tags = sHandler.tags_list
-    
+
     def __str__(self):
         full_fich = ""
         for tag in self.tags:
             t_name = tag.keys()[0]
             full_tag = t_name
             for attr in tag[t_name]:
-                value = tag[t_name][attr]    
+                value = tag[t_name][attr]
                 if value != "":
-                    full_tag += '\t' + attr + '=' + value   
+                    full_tag += '\t' + attr + '=' + value
             full_fich += full_tag + '\n'
         return full_fich
-    
+
     def do_local(self):
         for tag in self.tags:
             t_name = tag.keys()[0]
             full_tag = t_name
-            for attr in tag[t_name]: 
-                value = tag[t_name][attr] 
-                if attr == "src":                       
-                    os.system ('wget -q ' + value)
+            for attr in tag[t_name]:
+                value = tag[t_name][attr]
+                if attr == "src":
+                    os.system('wget -q ' + value)
                     tag[t_name][attr] = value.split('/')[-1]
- 
-try:
-    fichero = sys.argv[1]
-except IndexError:
-    print 'Usage: python karaoke.py file.smil'
-    raise SystemExit
 
-K = KaraokeLocal(fichero)
-print K
-K.do_local()
-print K
+
+if __name__ == "__main__":
+        try:
+            fichero = sys.argv[1]
+        except IndexError:
+            print 'Usage: python karaoke.py file.smil'
+            raise SystemExit
+
+        K = KaraokeLocal(fichero)
+        print K
+        K.do_local()
+        print K
--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,18 +4,19 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
         self.tags_list = []
 
     def get_tags(self, name, attrs):
         add = 1
         if name == 'root-layout':
             root_layout = {}
-            root_layout['width'] = attrs.get('width',"")            
-            root_layout['height'] = attrs.get('height',"")
-            root_layout['background-color'] = attrs.get('background-color',"")
+            root_layout['width'] = attrs.get('width', "")
+            root_layout['height'] = attrs.get('height', "")
+            root_layout['background-color'] = attrs.get('background-color', "")
             actual = {'root-layout': root_layout}
 
         elif name == 'region':
@@ -26,34 +27,34 @@ class SmallSMILHandler(ContentHandler):
             region['left'] = attrs.get('left', "")
             region['right'] = attrs.get('right', "")
             actual = {'region': region}
-                   
+
         elif name == 'img':
             img = {}
             img['src'] = attrs.get('src', "")
             img['region'] = attrs.get('region', "")
             img['begin'] = attrs.get('begin', "")
             img['dur'] = attrs.get('dur', "")
-            actual = {'img': img} 
-        
+            actual = {'img': img}
+
         elif name == 'audio':
             audio = {}
             audio['src'] = attrs.get('src', "")
             audio['begin'] = attrs.get('begin', "")
-            audio['dur'] = attrs.get('dur', "")             
+            audio['dur'] = attrs.get('dur', "")
             actual = {'audio': audio}
-        elif name == 'textstream':     
-            textstream = {} 
+        elif name == 'textstream':
+            textstream = {}
             textstream['src'] = attrs.get('src', "")
             textstream['region'] = attrs.get('region', "")
-            actual = {'textstream': textstream} 
+            actual = {'textstream': textstream}
         else:
             add = 0
 
         if add:
             self.tags_list.append(actual)
-    
+
     def startElement(self, name, attrs):
-        self.get_tags(name, attrs)  
+        self.get_tags(name, attrs)
 
 
 if __name__ == "__main__":
